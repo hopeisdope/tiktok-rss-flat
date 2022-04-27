8d38f267d8c5d114e140b2f732df9c69bf16163d
hopeisdope <nikyhoedemakers@gmail.com>
2022-04-27 13:27:46 +0200

    Update postprocessing.py

--- postprocessing.py
+++ postprocessing.py
@@ -4,12 +4,12 @@ from feedgen.feed import FeedGenerator
 from datetime import datetime, timezone
 
 # Normal GitHub Pages URL
-# ghPagesURL = "https://conoro.github.io/tiktok-rss-flat/"
+# ghPagesURL = "https://hopeisdope.github.io/tiktok-rss-flat/"
 
 # Custom Domain
-ghPagesURL = "https://tiktokrss.conoroneill.com/"
+ghPagesURL = "https://hopeisdope.github.io/tiktok-rss-flat/"
 
-api = TikTokApi()
+api = TikTokApi.get_instance()
 
 count = 10
 
@@ -18,33 +18,27 @@ with open('subscriptions.csv') as f:
     for row in cf:
         user = row['username']
 
-        print(f'Running for user \'{user}\'')
+        print (user)
 
+        tiktoks = api.by_username(user, count=count)
+        
         fg = FeedGenerator()
         fg.id('https://www.tiktok.com/@' + user)
         fg.title(user + ' TikTok')
         fg.author( {'name':'Conor ONeill','email':'conor@conoroneill.com'} )
         fg.link( href='http://tiktok.com', rel='alternate' )
-        fg.logo(ghPagesURL + 'tiktok-rss.png')
+        fg.logo(ghPagesURL + '/tiktok-rss.png')
         fg.subtitle('OK Boomer, all the latest TikToks from ' + user)
         fg.link( href=ghPagesURL + 'rss/' + user + '.xml', rel='self' )
         fg.language('en')
 
-        # Set the last modification time for the feed to be the most recent post, else now.
-        updated=None
-
-        for tiktok in api.user(username=user).videos(count=count):
+        for tiktok in tiktoks:
             fe = fg.add_entry()
-            link = "https://tiktok.com/@" + user + "/video/" + tiktok.id
+            link = "https://www.tiktok.com/@" + user + "/video/" + tiktok['id']
             fe.id(link)
-            ts = datetime.fromtimestamp(tiktok.as_dict['createTime'], timezone.utc)
-            fe.published(ts)
-            fe.updated(ts)
-            updated = max(ts, updated) if updated else ts
-            fe.title(tiktok.as_dict['desc'])
+            fe.published(datetime.fromtimestamp(tiktok['createTime'], timezone.utc))
+            fe.title(tiktok['desc'])
             fe.link(href=link)
-            fe.description("<img src='" + tiktok.as_dict['video']['cover'] + "' />")
+            fe.description("<img src='" + tiktok['video']['cover'] + "' />")
 
-        fg.updated(updated)
-
-        fg.atom_file('rss/' + user + '.xml', pretty=True) # Write the RSS feed to a file
+        fg.rss_file('rss/' + user + '.xml') # Write the RSS feed to a file
